4db63506148d1fdaf1522a6c83832b130f26cd84
tonovosel <tonovosel@foi.hr>
2014-05-28 18:29:42 +0200

    Dodan EDM

---
+++ Projekt/Skladiste_ETI/Skladiste_ETI/SkladisteModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\Techo\Documents\GitHub\RepositoryTeam02\Projekt\Skladiste_ETI\Skladiste_ETI\SkladisteModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.

9aaa4ef291e5b9c9d53bbe232f49c2c5a5f37e40
tonovosel <tonovosel@foi.hr>
2014-06-11 23:01:27 +0200

    Započet pregled dokumenata

--- Projekt/Skladiste_ETI/Skladiste_ETI/Dokumenti/frmPregledDokumenata.Designer.cs
+++ Projekt/Skladiste_ETI/Skladiste_ETI/Dokumenti/frmPregledDokumenata.Designer.cs
@@ -30,6 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPregledDokumenata));
             this.dgvDokumenti = new System.Windows.Forms.DataGridView();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDokumenti)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,24 +41,56 @@
             this.dgvDokumenti.AllowUserToAddRows = false;
             this.dgvDokumenti.AllowUserToDeleteRows = false;
             this.dgvDokumenti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvDokumenti.Location = new System.Drawing.Point(56, 55);
+            this.dgvDokumenti.Location = new System.Drawing.Point(185, 12);
             this.dgvDokumenti.Name = "dgvDokumenti";
             this.dgvDokumenti.ReadOnly = true;
             this.dgvDokumenti.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvDokumenti.Size = new System.Drawing.Size(852, 271);
+            this.dgvDokumenti.Size = new System.Drawing.Size(879, 352);
             this.dgvDokumenti.TabIndex = 0;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(51, 61);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 1;
+            this.btnPrint.Text = "Ispis";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(51, 132);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(75, 23);
+            this.btnChange.TabIndex = 2;
+            this.btnChange.Text = "Izmjena";
+            this.btnChange.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(51, 201);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Izlaz";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // frmPregledDokumenata
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(959, 376);
+            this.ClientSize = new System.Drawing.Size(1076, 376);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnPrint);
             this.Controls.Add(this.dgvDokumenti);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmPregledDokumenata";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Pregled dokumenata";
+            this.Load += new System.EventHandler(this.frmPregledDokumenata_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvDokumenti)).EndInit();
             this.ResumeLayout(false);
 
@@ -64,5 +99,8 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgvDokumenti;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnExit;
     }
 }
--- Projekt/Skladiste_ETI/Skladiste_ETI/Dokumenti/frmPregledDokumenata.cs
+++ Projekt/Skladiste_ETI/Skladiste_ETI/Dokumenti/frmPregledDokumenata.cs
@@ -12,18 +12,49 @@ namespace Skladiste_ETI.Dokumenti
 {
     public partial class frmPregledDokumenata : Form
     {
+        T02_DBEntities context = new T02_DBEntities();
         public frmPregledDokumenata()
         {
             InitializeComponent();
         }
 
-        
-        public void PrikaziDokumente() 
+        public class dokument 
         {
-           
-        }
-          
+            public int id_dokumenta { get; set; }
+            public string naziv_partnera { get; set; }
+            public string adresa_partnera { get; set; }
+            public string ime_skladistara { get; set; }
+            public string prez_skladistara { get; set; }
+            public DateTime datum { get; set; }
+            public string osnova { get; set; }
+            public string nacin_trans { get; set; }
+            public string stanje { get; set; }
+            public string tip { get; set; }
 
+        }
+
+        public class stavke 
+        {
+            public int id_stavke { get; set; }
+            public string naziv_artikla { get; set; }
+            public float kolicina { get; set; }
+            public float masa { get; set; }
+            public float cijena { get; set; }
+        }
+
+        private void frmPregledDokumenata_Load(object sender, EventArgs e)
+        {
+
+            
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+   
     }
 
 }

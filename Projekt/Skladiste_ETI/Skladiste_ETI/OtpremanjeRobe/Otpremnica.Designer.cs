47f34e94d9597113cdc512c8344543af805a7539
IvaBuconjic <1.3.osier.7@gmail.com>
2014-06-16 23:04:46 +0200

    Unos podataka otpremnice
    Unos podataka otpremnice bez artikla otpremnice.

--- Projekt/Skladiste_ETI/Skladiste_ETI/OtpremanjeRobe/Otpremnica.Designer.cs
+++ Projekt/Skladiste_ETI/Skladiste_ETI/OtpremanjeRobe/Otpremnica.Designer.cs
@@ -32,25 +32,25 @@
             this.btnExit = new System.Windows.Forms.Button();
             this.pbSlika = new System.Windows.Forms.PictureBox();
             this.gbOpisOtpremnice = new System.Windows.Forms.GroupBox();
-            this.gbArtikliZaOtpremu = new System.Windows.Forms.GroupBox();
-            this.lblZaTvrtku = new System.Windows.Forms.Label();
-            this.lblIzradio = new System.Windows.Forms.Label();
-            this.lblNacinTransporta = new System.Windows.Forms.Label();
-            this.lblPremaDokumentu = new System.Windows.Forms.Label();
-            this.lblDatum = new System.Windows.Forms.Label();
-            this.cmbPartner = new System.Windows.Forms.ComboBox();
-            this.cmbKorisnik = new System.Windows.Forms.ComboBox();
-            this.txtNacinDopreme = new System.Windows.Forms.TextBox();
-            this.cmbOsnova = new System.Windows.Forms.ComboBox();
-            this.dtpDatum = new System.Windows.Forms.DateTimePicker();
-            this.lblNaziv = new System.Windows.Forms.Label();
-            this.lblKolicina = new System.Windows.Forms.Label();
-            this.lblMasa = new System.Windows.Forms.Label();
-            this.cmbNazivArtikla = new System.Windows.Forms.ComboBox();
-            this.txtKolicina = new System.Windows.Forms.TextBox();
-            this.txtMasa = new System.Windows.Forms.TextBox();
             this.btnUnosOtpremnice = new System.Windows.Forms.Button();
+            this.dtpDatum = new System.Windows.Forms.DateTimePicker();
+            this.cmbOsnova = new System.Windows.Forms.ComboBox();
+            this.txtNacinDopreme = new System.Windows.Forms.TextBox();
+            this.cmbKorisnik = new System.Windows.Forms.ComboBox();
+            this.cmbPartner = new System.Windows.Forms.ComboBox();
+            this.lblDatum = new System.Windows.Forms.Label();
+            this.lblPremaDokumentu = new System.Windows.Forms.Label();
+            this.lblNacinTransporta = new System.Windows.Forms.Label();
+            this.lblIzradio = new System.Windows.Forms.Label();
+            this.lblZaTvrtku = new System.Windows.Forms.Label();
+            this.gbArtikliZaOtpremu = new System.Windows.Forms.GroupBox();
             this.btnUnosStavki = new System.Windows.Forms.Button();
+            this.txtMasa = new System.Windows.Forms.TextBox();
+            this.txtKolicina = new System.Windows.Forms.TextBox();
+            this.cmbNazivArtikla = new System.Windows.Forms.ComboBox();
+            this.lblMasa = new System.Windows.Forms.Label();
+            this.lblKolicina = new System.Windows.Forms.Label();
+            this.lblNaziv = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbSlika)).BeginInit();
             this.gbOpisOtpremnice.SuspendLayout();
             this.gbArtikliZaOtpremu.SuspendLayout();
@@ -66,6 +66,8 @@
             this.btnExit.Size = new System.Drawing.Size(103, 98);
             this.btnExit.TabIndex = 0;
             this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            this.btnExit.MouseHover += new System.EventHandler(this.btnExit_MouseHover);
             // 
             // pbSlika
             // 
@@ -98,6 +100,102 @@
             this.gbOpisOtpremnice.TabStop = false;
             this.gbOpisOtpremnice.Text = "Opis otpremnice";
             // 
+            // btnUnosOtpremnice
+            // 
+            this.btnUnosOtpremnice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUnosOtpremnice.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnUnosOtpremnice.Image = ((System.Drawing.Image)(resources.GetObject("btnUnosOtpremnice.Image")));
+            this.btnUnosOtpremnice.Location = new System.Drawing.Point(456, 134);
+            this.btnUnosOtpremnice.Name = "btnUnosOtpremnice";
+            this.btnUnosOtpremnice.Size = new System.Drawing.Size(75, 78);
+            this.btnUnosOtpremnice.TabIndex = 10;
+            this.btnUnosOtpremnice.UseVisualStyleBackColor = true;
+            this.btnUnosOtpremnice.Click += new System.EventHandler(this.btnUnosOtpremnice_Click);
+            this.btnUnosOtpremnice.MouseHover += new System.EventHandler(this.btnUnosOtpremnice_MouseHover);
+            // 
+            // dtpDatum
+            // 
+            this.dtpDatum.Location = new System.Drawing.Point(233, 150);
+            this.dtpDatum.Name = "dtpDatum";
+            this.dtpDatum.Size = new System.Drawing.Size(200, 22);
+            this.dtpDatum.TabIndex = 9;
+            // 
+            // cmbOsnova
+            // 
+            this.cmbOsnova.FormattingEnabled = true;
+            this.cmbOsnova.Location = new System.Drawing.Point(233, 118);
+            this.cmbOsnova.Name = "cmbOsnova";
+            this.cmbOsnova.Size = new System.Drawing.Size(200, 24);
+            this.cmbOsnova.TabIndex = 8;
+            // 
+            // txtNacinDopreme
+            // 
+            this.txtNacinDopreme.Location = new System.Drawing.Point(233, 89);
+            this.txtNacinDopreme.Name = "txtNacinDopreme";
+            this.txtNacinDopreme.Size = new System.Drawing.Size(200, 22);
+            this.txtNacinDopreme.TabIndex = 7;
+            // 
+            // cmbKorisnik
+            // 
+            this.cmbKorisnik.FormattingEnabled = true;
+            this.cmbKorisnik.Location = new System.Drawing.Point(233, 59);
+            this.cmbKorisnik.Name = "cmbKorisnik";
+            this.cmbKorisnik.Size = new System.Drawing.Size(200, 24);
+            this.cmbKorisnik.TabIndex = 6;
+            // 
+            // cmbPartner
+            // 
+            this.cmbPartner.FormattingEnabled = true;
+            this.cmbPartner.Location = new System.Drawing.Point(233, 30);
+            this.cmbPartner.Name = "cmbPartner";
+            this.cmbPartner.Size = new System.Drawing.Size(200, 24);
+            this.cmbPartner.TabIndex = 5;
+            // 
+            // lblDatum
+            // 
+            this.lblDatum.AutoSize = true;
+            this.lblDatum.Location = new System.Drawing.Point(25, 150);
+            this.lblDatum.Name = "lblDatum";
+            this.lblDatum.Size = new System.Drawing.Size(56, 16);
+            this.lblDatum.TabIndex = 4;
+            this.lblDatum.Text = "Datum:";
+            // 
+            // lblPremaDokumentu
+            // 
+            this.lblPremaDokumentu.AutoSize = true;
+            this.lblPremaDokumentu.Location = new System.Drawing.Point(25, 121);
+            this.lblPremaDokumentu.Name = "lblPremaDokumentu";
+            this.lblPremaDokumentu.Size = new System.Drawing.Size(199, 16);
+            this.lblPremaDokumentu.TabIndex = 3;
+            this.lblPremaDokumentu.Text = "Izrađeno prema dokumentu:";
+            // 
+            // lblNacinTransporta
+            // 
+            this.lblNacinTransporta.AutoSize = true;
+            this.lblNacinTransporta.Location = new System.Drawing.Point(25, 92);
+            this.lblNacinTransporta.Name = "lblNacinTransporta";
+            this.lblNacinTransporta.Size = new System.Drawing.Size(126, 16);
+            this.lblNacinTransporta.TabIndex = 2;
+            this.lblNacinTransporta.Text = "Način transporta:";
+            // 
+            // lblIzradio
+            // 
+            this.lblIzradio.AutoSize = true;
+            this.lblIzradio.Location = new System.Drawing.Point(25, 62);
+            this.lblIzradio.Name = "lblIzradio";
+            this.lblIzradio.Size = new System.Drawing.Size(141, 16);
+            this.lblIzradio.TabIndex = 1;
+            this.lblIzradio.Text = "Izradio (skladištar):";
+            // 
+            // lblZaTvrtku
+            // 
+            this.lblZaTvrtku.AutoSize = true;
+            this.lblZaTvrtku.Location = new System.Drawing.Point(25, 33);
+            this.lblZaTvrtku.Name = "lblZaTvrtku";
+            this.lblZaTvrtku.Size = new System.Drawing.Size(71, 16);
+            this.lblZaTvrtku.TabIndex = 0;
+            this.lblZaTvrtku.Text = "Za tvrtku:";
+            // 
             // gbArtikliZaOtpremu
             // 
             this.gbArtikliZaOtpremu.Controls.Add(this.btnUnosStavki);
@@ -116,149 +214,6 @@
             this.gbArtikliZaOtpremu.TabStop = false;
             this.gbArtikliZaOtpremu.Text = "Artikli za otpremu";
             // 
-            // lblZaTvrtku
-            // 
-            this.lblZaTvrtku.AutoSize = true;
-            this.lblZaTvrtku.Location = new System.Drawing.Point(25, 33);
-            this.lblZaTvrtku.Name = "lblZaTvrtku";
-            this.lblZaTvrtku.Size = new System.Drawing.Size(71, 16);
-            this.lblZaTvrtku.TabIndex = 0;
-            this.lblZaTvrtku.Text = "Za tvrtku:";
-            // 
-            // lblIzradio
-            // 
-            this.lblIzradio.AutoSize = true;
-            this.lblIzradio.Location = new System.Drawing.Point(25, 62);
-            this.lblIzradio.Name = "lblIzradio";
-            this.lblIzradio.Size = new System.Drawing.Size(141, 16);
-            this.lblIzradio.TabIndex = 1;
-            this.lblIzradio.Text = "Izradio (skladištar):";
-            // 
-            // lblNacinTransporta
-            // 
-            this.lblNacinTransporta.AutoSize = true;
-            this.lblNacinTransporta.Location = new System.Drawing.Point(25, 92);
-            this.lblNacinTransporta.Name = "lblNacinTransporta";
-            this.lblNacinTransporta.Size = new System.Drawing.Size(126, 16);
-            this.lblNacinTransporta.TabIndex = 2;
-            this.lblNacinTransporta.Text = "Način transporta:";
-            // 
-            // lblPremaDokumentu
-            // 
-            this.lblPremaDokumentu.AutoSize = true;
-            this.lblPremaDokumentu.Location = new System.Drawing.Point(25, 121);
-            this.lblPremaDokumentu.Name = "lblPremaDokumentu";
-            this.lblPremaDokumentu.Size = new System.Drawing.Size(199, 16);
-            this.lblPremaDokumentu.TabIndex = 3;
-            this.lblPremaDokumentu.Text = "Izrađeno prema dokumentu:";
-            // 
-            // lblDatum
-            // 
-            this.lblDatum.AutoSize = true;
-            this.lblDatum.Location = new System.Drawing.Point(25, 150);
-            this.lblDatum.Name = "lblDatum";
-            this.lblDatum.Size = new System.Drawing.Size(56, 16);
-            this.lblDatum.TabIndex = 4;
-            this.lblDatum.Text = "Datum:";
-            // 
-            // cmbPartner
-            // 
-            this.cmbPartner.FormattingEnabled = true;
-            this.cmbPartner.Location = new System.Drawing.Point(233, 30);
-            this.cmbPartner.Name = "cmbPartner";
-            this.cmbPartner.Size = new System.Drawing.Size(200, 24);
-            this.cmbPartner.TabIndex = 5;
-            // 
-            // cmbKorisnik
-            // 
-            this.cmbKorisnik.FormattingEnabled = true;
-            this.cmbKorisnik.Location = new System.Drawing.Point(233, 59);
-            this.cmbKorisnik.Name = "cmbKorisnik";
-            this.cmbKorisnik.Size = new System.Drawing.Size(200, 24);
-            this.cmbKorisnik.TabIndex = 6;
-            // 
-            // txtNacinDopreme
-            // 
-            this.txtNacinDopreme.Location = new System.Drawing.Point(233, 89);
-            this.txtNacinDopreme.Name = "txtNacinDopreme";
-            this.txtNacinDopreme.Size = new System.Drawing.Size(200, 22);
-            this.txtNacinDopreme.TabIndex = 7;
-            // 
-            // cmbOsnova
-            // 
-            this.cmbOsnova.FormattingEnabled = true;
-            this.cmbOsnova.Location = new System.Drawing.Point(233, 118);
-            this.cmbOsnova.Name = "cmbOsnova";
-            this.cmbOsnova.Size = new System.Drawing.Size(200, 24);
-            this.cmbOsnova.TabIndex = 8;
-            // 
-            // dtpDatum
-            // 
-            this.dtpDatum.Location = new System.Drawing.Point(233, 150);
-            this.dtpDatum.Name = "dtpDatum";
-            this.dtpDatum.Size = new System.Drawing.Size(200, 22);
-            this.dtpDatum.TabIndex = 9;
-            // 
-            // lblNaziv
-            // 
-            this.lblNaziv.AutoSize = true;
-            this.lblNaziv.Location = new System.Drawing.Point(25, 32);
-            this.lblNaziv.Name = "lblNaziv";
-            this.lblNaziv.Size = new System.Drawing.Size(51, 16);
-            this.lblNaziv.TabIndex = 0;
-            this.lblNaziv.Text = "Naziv:";
-            // 
-            // lblKolicina
-            // 
-            this.lblKolicina.AutoSize = true;
-            this.lblKolicina.Location = new System.Drawing.Point(25, 67);
-            this.lblKolicina.Name = "lblKolicina";
-            this.lblKolicina.Size = new System.Drawing.Size(67, 16);
-            this.lblKolicina.TabIndex = 1;
-            this.lblKolicina.Text = "Količina:";
-            // 
-            // lblMasa
-            // 
-            this.lblMasa.AutoSize = true;
-            this.lblMasa.Location = new System.Drawing.Point(25, 98);
-            this.lblMasa.Name = "lblMasa";
-            this.lblMasa.Size = new System.Drawing.Size(50, 16);
-            this.lblMasa.TabIndex = 2;
-            this.lblMasa.Text = "Masa:";
-            // 
-            // cmbNazivArtikla
-            // 
-            this.cmbNazivArtikla.FormattingEnabled = true;
-            this.cmbNazivArtikla.Location = new System.Drawing.Point(111, 32);
-            this.cmbNazivArtikla.Name = "cmbNazivArtikla";
-            this.cmbNazivArtikla.Size = new System.Drawing.Size(121, 24);
-            this.cmbNazivArtikla.TabIndex = 3;
-            // 
-            // txtKolicina
-            // 
-            this.txtKolicina.Location = new System.Drawing.Point(111, 66);
-            this.txtKolicina.Name = "txtKolicina";
-            this.txtKolicina.Size = new System.Drawing.Size(121, 22);
-            this.txtKolicina.TabIndex = 4;
-            // 
-            // txtMasa
-            // 
-            this.txtMasa.Location = new System.Drawing.Point(111, 95);
-            this.txtMasa.Name = "txtMasa";
-            this.txtMasa.Size = new System.Drawing.Size(121, 22);
-            this.txtMasa.TabIndex = 5;
-            // 
-            // btnUnosOtpremnice
-            // 
-            this.btnUnosOtpremnice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnUnosOtpremnice.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.btnUnosOtpremnice.Image = ((System.Drawing.Image)(resources.GetObject("btnUnosOtpremnice.Image")));
-            this.btnUnosOtpremnice.Location = new System.Drawing.Point(456, 134);
-            this.btnUnosOtpremnice.Name = "btnUnosOtpremnice";
-            this.btnUnosOtpremnice.Size = new System.Drawing.Size(75, 78);
-            this.btnUnosOtpremnice.TabIndex = 10;
-            this.btnUnosOtpremnice.UseVisualStyleBackColor = true;
-            // 
             // btnUnosStavki
             // 
             this.btnUnosStavki.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -269,6 +224,56 @@
             this.btnUnosStavki.Size = new System.Drawing.Size(75, 77);
             this.btnUnosStavki.TabIndex = 6;
             this.btnUnosStavki.UseVisualStyleBackColor = true;
+            this.btnUnosStavki.MouseHover += new System.EventHandler(this.btnUnosStavki_MouseHover);
+            // 
+            // txtMasa
+            // 
+            this.txtMasa.Location = new System.Drawing.Point(111, 95);
+            this.txtMasa.Name = "txtMasa";
+            this.txtMasa.Size = new System.Drawing.Size(121, 22);
+            this.txtMasa.TabIndex = 5;
+            // 
+            // txtKolicina
+            // 
+            this.txtKolicina.Location = new System.Drawing.Point(111, 66);
+            this.txtKolicina.Name = "txtKolicina";
+            this.txtKolicina.Size = new System.Drawing.Size(121, 22);
+            this.txtKolicina.TabIndex = 4;
+            // 
+            // cmbNazivArtikla
+            // 
+            this.cmbNazivArtikla.FormattingEnabled = true;
+            this.cmbNazivArtikla.Location = new System.Drawing.Point(111, 32);
+            this.cmbNazivArtikla.Name = "cmbNazivArtikla";
+            this.cmbNazivArtikla.Size = new System.Drawing.Size(121, 24);
+            this.cmbNazivArtikla.TabIndex = 3;
+            // 
+            // lblMasa
+            // 
+            this.lblMasa.AutoSize = true;
+            this.lblMasa.Location = new System.Drawing.Point(25, 98);
+            this.lblMasa.Name = "lblMasa";
+            this.lblMasa.Size = new System.Drawing.Size(50, 16);
+            this.lblMasa.TabIndex = 2;
+            this.lblMasa.Text = "Masa:";
+            // 
+            // lblKolicina
+            // 
+            this.lblKolicina.AutoSize = true;
+            this.lblKolicina.Location = new System.Drawing.Point(25, 67);
+            this.lblKolicina.Name = "lblKolicina";
+            this.lblKolicina.Size = new System.Drawing.Size(67, 16);
+            this.lblKolicina.TabIndex = 1;
+            this.lblKolicina.Text = "Količina:";
+            // 
+            // lblNaziv
+            // 
+            this.lblNaziv.AutoSize = true;
+            this.lblNaziv.Location = new System.Drawing.Point(25, 32);
+            this.lblNaziv.Name = "lblNaziv";
+            this.lblNaziv.Size = new System.Drawing.Size(51, 16);
+            this.lblNaziv.TabIndex = 0;
+            this.lblNaziv.Text = "Naziv:";
             // 
             // frmOtpremnica
             // 
--- Projekt/Skladiste_ETI/Skladiste_ETI/OtpremanjeRobe/Otpremnica.cs
+++ Projekt/Skladiste_ETI/Skladiste_ETI/OtpremanjeRobe/Otpremnica.cs
@@ -12,9 +12,188 @@ namespace Skladiste_ETI.OtpremanjeRobe
 {
     public partial class frmOtpremnica : Form
     {
+        List<korisnik> zaposlenici = new List<korisnik>();
+        List<artikli> roba = new List<artikli>();
+        List<poslovni_partner> partner = new List<poslovni_partner>();
+        List<dokument> dokumentPrikaz = new List<dokument>();
+
+
         public frmOtpremnica()
         {
             InitializeComponent();
+
+
+            using (var db = new T02_DBEntities())
+            {
+                partner = db.poslovni_partner.AsEnumerable().Select(p => new poslovni_partner()   //napravljena nova lista sa id-om i nazivom
+                {
+                    idPartnera = p.id_partnera,
+                    pos_partner = p.id_partnera + " " + p.naziv
+                }).ToList();
+
+                cmbPartner.DataSource = partner;
+                cmbPartner.DisplayMember = "pos_partner";
+                cmbPartner.ValueMember = "idPartnera";
+
+
+                zaposlenici = db.korisnik.AsEnumerable().Select(z => new korisnik()   //napravljena nova lista sa imenom i prezimenom
+                {
+                    id_zaposlenika = z.id_korisnika,
+                    zaposlenik = z.id_korisnika + " " + z.ime + " " + z.prezime   //ubaci u listu ime i prezime
+                }).ToList();
+
+                cmbKorisnik.DataSource = zaposlenici;   //prikaži listu sa imenom i prezimenom zaposlenika
+                cmbKorisnik.DisplayMember = "zaposlenik";
+                cmbKorisnik.ValueMember = "id_zaposlenika";
+
+
+                roba = db.artikli.AsEnumerable().Select(a => new artikli()   //napravljena nova lista sa id-om i nazivom artikla
+                {
+                    ida = a.id_artikla,
+                    naziv_artikla = a.id_artikla + " | " + a.naziv + " | Na skladištu: " + a.kolicina   //ubaci u listu id, naziv i količinu na skladištu
+                }).ToList();
+
+                cmbNazivArtikla.DataSource = roba;   //prikazi listu sa id-om i nazivom artikla
+                cmbNazivArtikla.DisplayMember = "naziv_artikla";
+                cmbNazivArtikla.ValueMember = "ida";
+
+
+                dokumentPrikaz = db.dokument.AsEnumerable().Select(d => new dokument()
+                {
+                    idDok = d.id_dokumenta,
+                    doc = d.id_dokumenta + " | Izdatnica br. " + d.id_dokumenta
+                }).ToList();
+
+                cmbOsnova.DataSource = dokumentPrikaz;
+                cmbOsnova.DisplayMember = "doc";
+                cmbOsnova.ValueMember = "idDok";
+
+
+            }//using
+        }//konstruktor
+
+
+        public class korisnik
+        {
+            public int id_zaposlenika { get; set; }
+            public string zaposlenik { get; set; }
+        }
+
+        public class artikli
+        {
+            public int ida { get; set; }
+            public string naziv_artikla { get; set; }
+        }
+
+        public class poslovni_partner
+        {
+            public int idPartnera { get; set; }
+            public string pos_partner { get; set; }
+        }
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void btnExit_MouseHover(object sender, EventArgs e)
+        {
+            System.Windows.Forms.ToolTip toolTip1 = new System.Windows.Forms.ToolTip();
+            toolTip1.SetToolTip(this.btnExit, "Izlaz");
+        }
+
+
+        private void btnUnosOtpremnice_MouseHover(object sender, EventArgs e)
+        {
+            System.Windows.Forms.ToolTip toolTip2 = new System.Windows.Forms.ToolTip();
+            toolTip2.SetToolTip(this.btnUnosOtpremnice, "Unos podataka o otpremnici");
+        }
+
+
+        private void btnUnosStavki_MouseHover(object sender, EventArgs e)
+        {
+            System.Windows.Forms.ToolTip toolTip3 = new System.Windows.Forms.ToolTip();
+            toolTip3.SetToolTip(this.btnUnosStavki, "Unos stavki otpremnice");
+        }
+
+
+        private void btnUnosOtpremnice_Click(object sender, EventArgs e)
+        {
+            if (txtNacinDopreme.Text == "")
+            {
+                MessageBox.Show("Niste unijeli način transporta robe!");
+            }
+            else
+            {
+                using (var db = new T02_DBEntities())
+                {
+                    DateTime datum1;
+                    datum1 = dtpDatum.Value;
+                    string idKorisnika = "";
+                    string idPartnera1 = "";
+                    string idDokumenta = "";
+                    
+                    foreach (char a in cmbKorisnik.Text)
+                    {
+                        if (a == ' ') break;
+                        else
+                        {
+                            idKorisnika += a;
+                        }
+                    }
+
+                    foreach (char a in cmbPartner.Text)
+                    {
+                        if (a == ' ') break;
+                        else
+                        {
+                            idPartnera1 += a;
+                        }
+                    }
+
+                    foreach (char a in cmbOsnova.Text)
+                    {
+                        if (a == ' ') break;
+                        else
+                        {
+                            idDokumenta += a;
+                        }
+                    }
+
+                    string query = string.Format("SELECT stanje FROM dokument WHERE id_dokumenta = '{0}'", idDokumenta);
+                    string stanje = db.Database.SqlQuery<string>(query).FirstOrDefault<string>();
+
+
+                    if (stanje == "Odobrena")
+                    {
+                        dokument dokument = new dokument
+                        {
+                            korisnik_id_korisnika = int.Parse(idKorisnika),
+                            tip_dokumenta_id_tipa = 4,
+                            poslovni_partner_id_partnera = int.Parse(idPartnera1),
+                            datum = datum1,
+                            stanje = "Kreirana",
+                            osnova = cmbOsnova.Text,
+                            način_trans = txtNacinDopreme.Text
+                        };
+                        db.dokument.Add(dokument);
+                        db.SaveChanges();
+
+                        txtNacinDopreme.Text = "";
+                        MessageBox.Show("Otpremnica je uspješno unesena!");
+                    }//if
+                   
+                    else
+                    {
+                        MessageBox.Show("Ne možete napraviti otpremnicu na temelju izdatnice koja nije odobrena!");
+                    }
+
+                }//using
+
+            }//else
+
         }
     }
 }

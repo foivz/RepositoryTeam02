751572a9da935e44383710b2f177ec75f05fe26b
tonovosel <tonovosel@foi.hr>
2014-06-22 10:59:38 +0200

    Novi setup file nakon izmjene

--- Projekt/Skladiste_ETI/Skladiste_ETI/Properties/AssemblyInfo.cs
+++ Projekt/Skladiste_ETI/Skladiste_ETI/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Skladiste_ETI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Aplikacija za podšku u skladištu")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ETITeam")]
 [assembly: AssemblyProduct("Skladiste_ETI")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.34.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
